47f0b176786df255d3162c03eaf3bff42a41d338
hirotaka46 <hiroventolero@gmail.com>
2022-06-01 14:54:24 +0800

    Making a Sidebar

---
+++ components/sidebar.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Sidebar: React.FC = () => {
+    return (
+        <div className="flex flex-col ">
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+            <i>E</i>
+        </div>
+    )
+}
+
+export default Sidebar
