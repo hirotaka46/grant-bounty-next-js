c008575fb339cabd2989569184663943718a957b
hirotaka46 <hiroventolero@gmail.com>
2022-06-08 01:21:44 +0800

    Side bar contents

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -2,14 +2,14 @@ import React from "react"
 
 const Sidebar: React.FC = () => {
     return (
-        <div className="font-bold fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary">
+        <div className="font-bold fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-primary text-secondary border-2">
             <i>Home</i>
             <i>Dashboard</i>
-            <i>C</i>
-            <i>D</i>
-            <i>E</i>
+            <i>Contact Us</i>
+            <i>About</i>
         </div>
     )
 }
 
 export default Sidebar
+
